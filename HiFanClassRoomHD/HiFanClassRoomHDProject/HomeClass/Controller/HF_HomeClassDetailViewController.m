fcd44823d8b959083b5a5bbbe3a5e9801939eb16
何建新 <yiyuanhanly@126.com>
2018-12-20 09:04:42 +0800

    6668888

--- HiFanClassRoomHD/HiFanClassRoomHDProject/HomeClass/Controller/HF_HomeClassDetailViewController.m
+++ HiFanClassRoomHD/HiFanClassRoomHDProject/HomeClass/Controller/HF_HomeClassDetailViewController.m
@@ -7,10 +7,11 @@
 //
 
 #import "HF_HomeClassDetailViewController.h"
-
+#import "HF_ClassDetailsTopView.h"
 @interface HF_HomeClassDetailViewController ()
 //关闭
 @property (nonatomic, strong) UIButton *closeButton;
+
 @end
 
 @implementation HF_HomeClassDetailViewController
@@ -39,46 +40,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    //检测设备
-    UILabel *titleLabel = [[UILabel alloc]init];
-    titleLabel.font = [UIFont fontWithName:@"PingFangSC-Medium" size:LineX(20)];
-    titleLabel.textColor = UICOLOR_FROM_HEX_ALPHA(Color000000, 70);
-    titleLabel.text = @"课程详情";
-    [self.view addSubview:titleLabel];
-    
-    [titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.equalTo(self.view.mas_left).offset(17);
-        make.top.equalTo(self.view.mas_top).offset(26);
-        make.height.mas_offset(20);
-    }];
-    
-    //分割线
-    UIView *lineView = [[UIView alloc]init];
-    lineView.backgroundColor = UICOLOR_FROM_HEX(ColorEAEFF3);
-    [self.view addSubview:lineView];
-    
-    [lineView mas_makeConstraints:^(MASConstraintMaker *make) {
+    HF_ClassDetailsTopView *topView = [HF_ClassDetailsTopView new];
+    [self.view addSubview:topView];
+    [topView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(self.view.mas_left).offset(17);
         make.right.equalTo(self.view.mas_right).offset(-17);
-        make.top.equalTo(self.view.mas_top).offset(63);
-        make.height.mas_offset(1);
-    }];
-    
-    
-    
-    self.closeButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    [self.closeButton setImage:UIIMAGE_FROM_NAME(@"close") forState:(UIControlStateNormal)];
-    [self.view addSubview:self.closeButton];
-    
-    [self.closeButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.right.equalTo(self.view.mas_right).offset(-10);
-        make.top.equalTo(self.view.mas_top).offset(10);
-        make.size.mas_offset(CGSizeMake(50, 50));
+        make.top.equalTo(self.view.mas_top).offset(0);
+        make.height.mas_equalTo(LineH(245));
     }];
     
     @weakify(self);
-    [[self.closeButton rac_signalForControlEvents:UIControlEventTouchUpInside]
+    [[topView.closeButton rac_signalForControlEvents:UIControlEventTouchUpInside]
      subscribeNext:^(id x) {
          @strongify(self);
          [self dismissViewControllerAnimated:YES completion:nil];
